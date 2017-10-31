0d44d733f6f902c9ce9de33c40fcdcca35d0d5cd
David Rivero <davidmriverog@gmail.com>
2017-10-31 12:02:01 -0400

    eje003 - avances en el curso

---
+++ eje003/src/app/empleado/empleado.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html'
+})
+export class EmpleadoComponent {
+  public titulo = 'Mi empleado';
+}
